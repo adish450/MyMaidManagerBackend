c27ff86eded4295f791f26e2f879fcf5ded6675e
adish17 <adishirfan17@gmail.com>
2025-10-15 22:49:42 +0530

    fix server running log message

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // A simple test route to check if the server is running
 app.get('/', (req, res) => {
-  res.send('My Maid Manager API is running... demo text');
+  res.send('My Maid Manager API is running...');
 });
 
 // Import and use API routes
